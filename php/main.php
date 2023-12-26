0b23c3a2e16ee2af654129be2d3968eda9346b6b
FrancoVillarLaz <villarlazfranco@gmail.com>
2023-12-26 08:29:56 -0300

    Funcion Paginador
    -Creación función de paginador
          -Pagina siguiente y anterior
          -Pagina inicial y final

--- php/main.php
+++ php/main.php
@@ -61,4 +61,42 @@
         $nombre=$nombre."_".rand(0,1000);
         return $nombre;
     }
-    
+
+    //Funcion para el paginador de tablas
+
+    function paginadorTablas($paginaActual,$paginasTotal,$url,$botones){
+        $tabla='<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+
+        //primera pagina y pagina anterior 
+        if ($paginaActual<=1) {
+            $tabla.='
+            <a class="pagination-previous is-disabled" disabled >Anterior</a>
+			<ul class="pagination-list">
+            ';
+        }else{
+            $tabla.='
+            <a class="pagination-previous" href="'.$url.($paginaActual-1).'" >Anterior</a>
+			<ul class="pagination-list">
+            <li><a class="pagination-link" href="'.$url.'1">1</a></li>
+            <li><span class="pagination-ellipsis">&hellip;</span></li>'
+            ;
+        }
+
+        //siguiente pagina y ultima pagina
+        if ($paginaActual=$paginasTotal) {
+            $tabla.='
+            </ul>
+            <a class="pagination-next is-disabled" disabled >Siguiente</a>
+			<ul class="pagination-list">';
+        }else{
+            $tabla.='
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li><a class="pagination-link" href="'.$url.$paginasTotal.'1">'.$paginasTotal.'</a></li>
+            </ul>
+            <a class="pagination-next" href="'.$url.($paginaActual+1).'" >Siguiente</a>
+			<ul class="pagination-list">
+            ';
+        }
+        $tabla.='</nav>';
+        return $tabla;
+    }
